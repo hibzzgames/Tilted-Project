2cc599f02638deddc653f7d345e5f4b163d29bf4
sliptrixx <37605842+sliptrix@users.noreply.github.com>
2020-04-23 17:51:55 +0530

    Implemented simple visual debugger that appears on screen

---
+++ Assets/Scripts/VisualDebugger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VisualDebugger : MonoBehaviour
+{
+    [Tooltip("Text area of the debug text")]
+    [SerializeField]
+    private TextMeshProUGUI DebugTextMesh = null;
+
+    /// <summary>
+    /// Adds the given text to the end of the current log
+    /// </summary>
+    /// <param name="text"> The text to be added </param>
+    public void Log(string text)
+    {
+        DebugTextMesh.text = DebugTextMesh.text + "\n[" + Time.time + "] " + text;
+    }
+
+    /// <summary>
+    /// Clears the current log
+    /// </summary>
+    public void Clear()
+    {
+        DebugTextMesh.text = "";
+    }
+}
