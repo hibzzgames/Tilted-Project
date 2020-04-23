8f778f12b1323af3586ca7c9b6cc53377573547c
sliptrixx <37605842+sliptrix@users.noreply.github.com>
2020-04-23 17:52:58 +0530

    Added a property called paddle responsiveness
    Paddle is most responsive when this value is 0

--- Assets/Scripts/PaddleInput.cs
+++ Assets/Scripts/PaddleInput.cs
@@ -1,53 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
+﻿using UnityEngine;
 
 public class PaddleInput : MonoBehaviour
 {
-    [Header("Paddle properties")]
-    [Tooltip("[Tooltip not defined yet]")]
-    [SerializeField]
-    private float AngleSensitivity = 0.01f;
+	[Header("Paddle properties")]
+	[Tooltip("[Tooltip not defined yet]")]
+	[SerializeField]
+	private float PaddleSensitivity = 5.0f;
 
-    [SerializeField]
-    private TextMeshProUGUI debugText;
+	[Tooltip("The amount of motion ignored by the paddle")]
+	[SerializeField]
+	private float PaddleResponsiveness = 0.1f;
 
-    private float RotationRateY;
+	/// <summary>
+	/// Rotation rate on the y axis of the device
+	/// </summary>
+	private float rotationRateY;
 
-    private Vector3 expectedPosition;
+	/// <summary> 
+	/// Expected position of the paddle at the next frame 
+	/// </summary>
+	private Vector3 expectedPosition;
 
-    private void Start()
-    {
-        expectedPosition = transform.position;
-    }
+	// Called at the start of the scene
+	private void Start()
+	{
+		// Set expected position to be the transform's initial position
+		expectedPosition = transform.position;
+	}
 
-    // When the object is enabled
-    private void OnEnable()
-    {
-        // Enable the attitude sensor
-        Input.gyro.enabled = true;
-    }
+	// When the object is enabled
+	private void OnEnable()
+	{
+		// Enable the attitude sensor
+		Input.gyro.enabled = true;
+	}
 
-    // When the object is disabled
-    private void OnDisable()
-    {
-        // Disable the attitude sensor
-        Input.gyro.enabled = false;
-    }
+	// When the object is disabled
+	private void OnDisable()
+	{
+		// Disable the attitude sensor
+		Input.gyro.enabled = false;
+	}
 
-    private void FixedUpdate()
-    {
-        RotationRateY = Input.gyro.rotationRate.y;
+	// Called once every frame
+	private void Update()
+	{
+		// Get the rotation rate in the y axis of the device
+		rotationRateY = Input.gyro.rotationRate.y;
 
-        expectedPosition.x += RotationRateY * AngleSensitivity;
-        expectedPosition.x = Mathf.Clamp(expectedPosition.x, -2.8125f, 2.8125f);
+		// ignore moving the paddle if the intensity is not high enough
+		if (rotationRateY < PaddleResponsiveness && rotationRateY > -PaddleResponsiveness) { return; }
 
-        debugText.text = "Attitude: " + Input.gyro.attitude.eulerAngles
-            + "\nGravity: " + Input.gyro.gravity
-            + "\nRotation Rate: " + Input.gyro.rotationRate
-            + "\nExpected Position X: " + expectedPosition.x;
+		// Apply the rotation rate along with the paddle speed to calculate expected position.
+		// Expected position is clamped based on the screen size.
+		expectedPosition.x += rotationRateY * PaddleSensitivity * Time.deltaTime;
+		expectedPosition.x = Mathf.Clamp(expectedPosition.x, -2.8125f, 2.8125f);
 
-        transform.position = expectedPosition;
-    }
+		// Set the object's position to the expected position
+		transform.position = expectedPosition;
+	}
 }
