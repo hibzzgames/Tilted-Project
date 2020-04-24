f761db07850dc779b0c46e53113471b6a74963d8
sliptrixx <37605842+sliptrix@users.noreply.github.com>
2020-04-24 07:30:32 +0530

    Removed magic number for clamping
    Clamping is calculated based on screen ratio and main camera's orthographic size.

--- Assets/Scripts/PaddleInput.cs
+++ Assets/Scripts/PaddleInput.cs
@@ -21,11 +21,16 @@ public class PaddleInput : MonoBehaviour
 	/// </summary>
 	private Vector3 expectedPosition;
 
+	private float ClampDistance = 2.8125f;
+
 	// Called at the start of the scene
 	private void Start()
 	{
 		// Set expected position to be the transform's initial position
 		expectedPosition = transform.position;
+
+		// Calculate clamp distance based on screen
+		ClampDistance = Screen.width * Camera.main.orthographicSize / Screen.height; 
 	}
 
 	// When the object is enabled
@@ -54,7 +59,7 @@ public class PaddleInput : MonoBehaviour
 		// Apply the rotation rate along with the paddle speed to calculate expected position.
 		// Expected position is clamped based on the screen size.
 		expectedPosition.x += rotationRateY * PaddleSensitivity * Time.deltaTime;
-		expectedPosition.x = Mathf.Clamp(expectedPosition.x, -2.8125f, 2.8125f);
+		expectedPosition.x = Mathf.Clamp(expectedPosition.x, -ClampDistance, ClampDistance);
 
 		// Set the object's position to the expected position
 		transform.position = expectedPosition;
