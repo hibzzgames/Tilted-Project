7fff9f0ab45d49e3ea98025bb701b3242e54a872
sliptrixx <37605842+sliptrix@users.noreply.github.com>
2020-04-25 13:18:37 +0530

    Basic colision with wall and paddle are fully functional

--- Assets/Scripts/PongPhysics.cs
+++ Assets/Scripts/PongPhysics.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PongPhysics : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Header("Pong Properties")]
+	[Tooltip("The speed at which the pong travels")]
+	[SerializeField]
+	private float Speed = 5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Vector3 expectedRotation;
+
+	private void FixedUpdate()
+	{
+		transform.position += transform.right * Time.deltaTime * Speed;
+
+		// TODO: Remove this (doesn't belong here)
+		if(transform.position.y < -10.0f)
+		{
+			GetComponent<SetSpawnLocation>().ResetPosition();
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		// Check if the collider is tagged as an object that reflects the pong
+		if (other.CompareTag("Wall") || other.CompareTag("Paddle"))
+		{
+			// Get a reflected vector with other's right as the normal of reflection
+			Vector3 reflectedVector = Vector3.Reflect(transform.right, other.transform.right);
+
+			// get the direction of this vector
+			float angle =  Mathf.Atan2(reflectedVector.y, reflectedVector.x);
+			
+			// the angle needs to be converted to degrees, and is set as the expected 
+			// resolved rotation 
+			expectedRotation.z = Mathf.Rad2Deg * angle;
+        }
+
+        // if the object is tagged paddle, perform additional calculation
+        if(other.CompareTag("Paddle"))
+		{
+
+		}
+
+		transform.rotation = Quaternion.Euler(expectedRotation);
+	}
 }
