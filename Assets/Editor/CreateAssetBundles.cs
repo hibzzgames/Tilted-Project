54d4e1e7b4f5ca53f48a04a195a5cfd35132dc11
sliptrixx <37605842+sliptrix@users.noreply.github.com>
2020-04-28 09:28:08 +0530

    Basic theme loader added

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/StreamingAssets";
+        if(!Directory.Exists(Application.streamingAssetsPath))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None,
+            EditorUserBuildSettings.activeBuildTarget);
+    }
+}
---
+++ Assets/Scripts/BundledThemeLoader.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class BundledThemeLoader : MonoBehaviour
+{
+	[Header("Theme Properties")]
+	[Tooltip("The name of the theme to load as an asset bundle")]
+	[InspectorName("Theme Name")]
+	public string bundleName = "prototypetheme";
+
+	[Header("Object Properties")]
+	public string PongAssetName = "PongAsset";
+	public string PaddleAssetName = "PaddleAsset";
+	public string BackgroundAssetName = "BackgroundAsset";
+
+	[Header("Gameobject References")]
+	public GameObject Pong = null;
+	public GameObject Paddle = null;
+	public GameObject Background = null;
+
+	private IEnumerator Start()
+	{
+		AssetBundleCreateRequest asyncBundleRequest = AssetBundle.LoadFromFileAsync(
+			Path.Combine(Application.streamingAssetsPath, bundleName));
+
+		yield return asyncBundleRequest;
+
+		AssetBundle localAssetBundle = asyncBundleRequest.assetBundle;
+
+		if(localAssetBundle == null)
+		{
+			Debug.LogError("Failed to load asset bundle of type " + bundleName);
+		}
+
+		AssetBundleRequest assetRequest = localAssetBundle.LoadAssetAsync<GameObject>(PongAssetName);
+		yield return assetRequest;
+
+		GameObject PongAsset = assetRequest.asset as GameObject;
+		Instantiate(PongAsset, Pong.transform);
+
+		assetRequest = localAssetBundle.LoadAssetAsync<GameObject>(PaddleAssetName);
+		yield return assetRequest;
+
+		GameObject PaddleAsset = assetRequest.asset as GameObject;
+		Instantiate(PaddleAsset, Paddle.transform);
+
+		assetRequest = localAssetBundle.LoadAssetAsync<GameObject>(BackgroundAssetName);
+		yield return assetRequest;
+
+		GameObject BackgroundAsset = assetRequest.asset as GameObject;
+		Instantiate(BackgroundAsset, Background.transform);
+
+		localAssetBundle.Unload(false);
+	}
+}
